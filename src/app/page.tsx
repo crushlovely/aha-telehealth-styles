c13495fe5727adf27e2e2d48c5cce25d5ef19ddc
jeffschram <schramindustries@gmail.com>
2025-09-03 16:09:26 -0400

    added stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,26 @@ import { NavCards } from "@/components/nav-cards";
 import { HowItWorks } from "@/components/how-it-works";
 import { FullWidthTestimonial } from "@/components/full-width-testimonial";
 import { Footer } from "@/components/footer";
+import { Stats } from "@/components/stats";
 import { SpeedBump } from "@/components/speed-bump";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Stats
+        title="Distinguished Advisory Committee"
+        description="Our esteemed advisory committee comprises the nation's most distinguished healthcare leaders, researchers, and innovators who guide our mission to advance telehealth science and practice through their unparalleled expertise."
+        stat1={{
+          number: "6",
+          description: "Distinguished Advisors",
+        }}
+        stat2={{
+          number: "200+",
+          description: "Combined Years Experience",
+        }}
+        stat3={{ number: "15M+", description: "Patients Impacted" }}
+      />
       <HeroFullWidth
         theme="aqua"
         image="/images/hero-homepage.jpg"
@@ -27,6 +41,19 @@ export default function Home() {
         title="Ready to Apply This Science?"
         body="Transform research into practice with our evidence-based training courses designed for healthcare professionals."
         button1="Explore Training Courses"
+      />
+      <Stats
+        stat1={{
+          number: "74%",
+          description:
+            "Physicians reporting telehealth use in their medical practices, according to the American Medical Association",
+        }}
+        stat2={{
+          number: "330K+",
+          description:
+            "Individuals have come to the American Heart Association Professional Education Hub for their education needs",
+        }}
+        stat3={{ number: "20M", description: "People trained annually in CPR" }}
       />
       <Footer />
       <SpeedBump open={false} />
